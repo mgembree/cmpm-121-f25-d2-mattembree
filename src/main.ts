d969de84decc75283fff3c447cd7b96362fa6903
mgembree <mgembree@ucsc.edu>
2025-10-17 00:39:59 +0000

    Completed Step 2
    Used Paint0Html starter code to create mouse button event handlers.
    
    Used these event handlers to draw onto canvas.
    
    Created clear button to clear canvas

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,43 @@ import "./style.css";
 document.body.innerHTML = `
 <h1>Sketchpad</h1>
 <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-<canvas id="sketchPad" width="256" height="256"></canvas>
 `;
+"use strict";
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+document.body.append(canvas);
+
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
